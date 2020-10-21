c60a3d91b3a81dff7f6d0151b9d7b4c151d85a35
aben20807 <aben20807@gmail.com>
2020-10-21 10:57:02 +0800

    Update install theme

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 mkdir ~/.oh-my-bash/themes/ouo/
-cp ./themes/ouo/ouo.theme.sh ~/.oh-my-bash/themes/ouo/
+curl -fLo ~/.oh-my-bash/themes/ouo/ouo.theme.sh https://raw.githubusercontent.com/aben20807/oh-my-ouo/master/themes/ouo/ouo.theme.sh
 
 sed -i 's/^\(OSH_THEME\s*=\s*\).*$/\1\"ouo\"/' .bashrc
 exit 0
